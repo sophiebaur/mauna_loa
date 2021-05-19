7ca1d5b35536d960591be0c599fac07e5d4dfa19
sophiebaur <sophienbaur@gmail.com>
2021-05-19 19:25:20 +0000

    Functions completed

--- ml.py
+++ ml.py
@@ -7,7 +7,11 @@ def get_df(filename):
     INPUT:filename e.g. mauna_loa.csv
     OUTPUT: Pandas Dataframe
     '''
-    ...
+    df=pd.read_csv(filename)
+    first_date=(df['decimal_date'][0])
+    df['years_since_first']=df['decimal_date']-first_date
+    df=df[['years_since_first','C02']]
+    return df
    
 
 def plot_df(df):
@@ -15,5 +19,15 @@ def plot_df(df):
     INPUT: Pandas DataFrame
     OUTPUT: handle to plot axis
     '''
+    plt.plot(df['years_since_first'],df['C02'])
+    plt.xlabel('Years since 1958')
+    plt.ylabel('CO2 levels')
+    
+    ax=plt.gca()
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    
+    
+    
     
     
